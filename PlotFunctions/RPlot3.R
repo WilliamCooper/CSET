3be5e385a125bce00c4d41110d5c5bbc77660d0a
WilliamCooper <cooperw@ucar.edu>
2015-06-26 11:14:14 -0600

    Changed ATHR references to ATH to match CJW changes in processing; also RTHR to RTH

--- PlotFunctions/RPlot3.R
+++ PlotFunctions/RPlot3.R
@@ -1,14 +1,14 @@
 ### plot 3: plot all temperatures, one plot
 RPlot3 <- function (data) { 
-  ## needs ATHR1, ATHR2, AT_A
+  ## needs ATH1, ATH2, AT_A
   par(oma=c(1.1,0,0,0))
   ylb <- expression (paste ("temperature  ATy  [", degree, "C]"))
-  plotWAC (data[, c("Time", "ATHR2", "ATHR1", "AT_A")],
+  plotWAC (data[, c("Time", "ATH2", "ATH1", "AT_A")],
            ylab=ylb, lty=c(1,1,2,1), 
            lwd=c(2,1.5,1,2), legend.position=NA) # pulling legend out of plotWAC to increase font size
-  legend('bottomright',c("ATHR2", "ATHR1", "AT_A"),col=c("blue","darkgreen","red"),text.col=c("blue","darkgreen","red"),lty=c(1,1,2),lwd=c(2,1.5,1))
-  title(sprintf("Means L1-L2: %.2f; L2-_A: %.2f", 
-                mean (data$ATHR1-data$ATHR2, na.rm=TRUE), 
-                mean (data$ATHR2-data$AT_A, na.rm=TRUE)), cex.main=0.8)
+  legend('bottomright',c("ATH2", "ATH1", "AT_A"),col=c("blue","darkgreen","red"),text.col=c("blue","darkgreen","red"),lty=c(1,1,2),lwd=c(2,1.5,1))
+  title(sprintf("Means H1-H2: %.2f; H1-_A: %.2f", 
+                mean (data$ATH1-data$ATH2, na.rm=TRUE), 
+                mean (data$ATH1-data$AT_A, na.rm=TRUE)), cex.main=0.8)
   AddFooter ()
 }
--- PlotFunctions/RPlot4.R
+++ PlotFunctions/RPlot4.R
@@ -1,73 +1,73 @@
 ### plot 4: plot differences, individual pairs of temperatures
 RPlot4 <- function (data) { 
-  ## needs ATHR1, ATHR2, AT_A
+  ## needs ATH1, ATH2, AT_A
   op <- par (mar=c(5,5,2,4)+0.1,oma=c(1.1,0,0,0))
   labelled=F
   layout(matrix(1:4, nrow=2, ncol = 2), widths = c(5,5), heights = c(5,6))
-  # ATHR1,2 section
+  # ATH1,2 section
   # protection against all-bad:
-  if (any(!is.na(data$ATHR1) & !is.na(data$ATHR2))) {
-    ylb <- expression (paste ("ATHR2  [", degree, "C]"))
-    plot (DF <- data[, c("ATHR1", "ATHR2")], pch=20,ylab=ylb)
+  if (any(!is.na(data$ATH1) & !is.na(data$ATH2))) {
+    ylb <- expression (paste ("ATH2  [", degree, "C]"))
+    plot (DF <- data[, c("ATH1", "ATH2")], pch=20,ylab=ylb)
     lines (c(-70.,30.), c(-69.,31.), col="darkorange", lwd=2, lty=2)
     lines (c(-70.,30.), c(-71.,29.), col="darkorange", lwd=2, lty=2)
     par(new=T)
-    plot (data$ATHR1, data$ATHR2 - data$ATHR1, type='l', col='red',axes=F,xlab='',ylab='',ylim=c(-2,2))
+    plot (data$ATH1, data$ATH2 - data$ATH1, type='l', col='red',axes=F,xlab='',ylab='',ylim=c(-2,2))
     axis (4,col='red',col.axis='red')
     abline(h=c(-1,1),col='red',lty=2)
-    mtext(expression (paste ("ATHR2 - ATHR1  [", degree, "C]")),4,3,col='red',cex=0.8)
-#    DF$ATHR2 <- (data$ATHR2 - data$ATHR1)*5
+    mtext(expression (paste ("ATH2 - ATH1  [", degree, "C]")),4,3,col='red',cex=0.8)
+#    DF$ATH2 <- (data$ATH2 - data$ATH1)*5
 #    points (DF, col='red', type='l', lwd=2)
 #    lines (c(-70,30), c(5,5), col='red', lty=2)
 #    lines (c(-70,30), c(-5,-5), col='red', lty=2)
 # this info somewhat redundant with legend now, also cex=0.5 was too small to read in pngs but 1.0 too big for plot
-#    legend ("bottomright", legend=c("red: y=(ATHR2-ATHR1)*5", 
+#    legend ("bottomright", legend=c("red: y=(ATH2-ATH1)*5", 
 #                                    "dashed lines: +/-1C error bands"), 
 #            box.col='red', text.col='red', cex=0.5)
-    fm <- lm(ATHR2~ATHR1, data=data)
+    fm <- lm(ATH2~ATH1, data=data)
     coef <- coefficients (fm)
     if (coef[1] < 0.) {
-      t <- sprintf ("ATHR2=%.3f(ATHR1)%.3f\nmean diff ATHR2-ATHR1=%.2f +/- %.2f", 
-                    coef[2], coef[1], mean (data$ATHR2-data$ATHR1, na.rm=TRUE),
-                    sd(data$ATHR2-data$ATHR1, na.rm=TRUE))
+      t <- sprintf ("ATH2=%.3f(ATH1)%.3f\nmean diff ATH2-ATH1=%.2f +/- %.2f", 
+                    coef[2], coef[1], mean (data$ATH2-data$ATH1, na.rm=TRUE),
+                    sd(data$ATH2-data$ATH1, na.rm=TRUE))
     } else {
-      t <- sprintf ("ATHR2=%.3f(ATHR1)+%.3f\nmean diff ATHR2-ATHR1=%.2f +/-%.2f", 
-                    coef[2], coef[1], mean (data$ATHR2-data$ATHR1, na.rm=TRUE),
-                    sd(data$ATHR2-data$ATHR1, na.rm=TRUE))
+      t <- sprintf ("ATH2=%.3f(ATH1)+%.3f\nmean diff ATH2-ATH1=%.2f +/-%.2f", 
+                    coef[2], coef[1], mean (data$ATH2-data$ATH1, na.rm=TRUE),
+                    sd(data$ATH2-data$ATH1, na.rm=TRUE))
     }
     title(t, cex.main=0.75)
     AddFooter ()
   }
   # AT_A section:
-  if (any(!is.na(data$AT_A) & !is.na(data$ATHR2))) {
+  if (any(!is.na(data$AT_A) & !is.na(data$ATH2))) {
     ylb <- expression (paste ("AT_A  [", degree, "C]"))
-    plot (DF <- data[, c("ATHR2", "AT_A")], pch=20,ylab=ylb)
+    plot (DF <- data[, c("ATH2", "AT_A")], pch=20,ylab=ylb)
     lines (c(-70.,30.), c(-69.,31.), col="darkorange", lwd=2, lty=2)
     lines (c(-70.,30.), c(-71.,29.), col="darkorange", lwd=2, lty=2)
     par(new=T)
-#### this is plotting ATHR2-AT_A but reporting in the title AT_A-ATHR2, should pick either and make consistent
-    plot (data$ATHR2, data$ATHR2 - data$AT_A, type='l', col='red',axes=F,xlab='',ylab='',ylim=c(-2,2))
+#### this is plotting ATH2-AT_A but reporting in the title AT_A-ATH2, should pick either and make consistent
+    plot (data$ATH2, data$ATH2 - data$AT_A, type='l', col='red',axes=F,xlab='',ylab='',ylim=c(-2,2))
     axis (4,col='red',col.axis='red')
     abline(h=c(-1,1),col='red',lty=2)
-    mtext(expression (paste ("ATHR2 - AT_A  [", degree, "C]")),4,3,col='red',cex=0.8)
-#    DF$AT_A <- (data$ATHR2 - data$AT_A)*5
+    mtext(expression (paste ("ATH2 - AT_A  [", degree, "C]")),4,3,col='red',cex=0.8)
+#    DF$AT_A <- (data$ATH2 - data$AT_A)*5
 #    points (DF, col='red', type='l', lwd=2)
 #    lines (c(-70,30), c(5,5), col='red', lty=2)
 ##    lines (c(-70,30), c(-5,-5), col='red', lty=2)
 # this info somewhat redundant with legend now, also cex=0.5 was too small to read in pngs but 1.0 too big for plot
-#    legend ("topleft", legend=c("red: y=(ATHR2-AT_A)*5", 
+#    legend ("topleft", legend=c("red: y=(ATH2-AT_A)*5", 
 #                                "dashed lines: +/-1C error bands"), 
 #            box.col='red', text.col='red', cex=0.5)
-    fm <- lm(AT_A~ATHR2, data=data)
+    fm <- lm(AT_A~ATH2, data=data)
     coef <- coefficients (fm)
     if (coef[1] < 0.) {
-      t <- sprintf ("AT_A=%.3f(ATHR2)%.3f\n mean diff AT_A-ATHR2%.2f +/-%.2f", 
-                    coef[2], coef[1], mean(data$AT_A-data$ATHR2, na.rm=TRUE),
-                    sd(data$AT_A-data$ATHR2, na.rm=TRUE))
+      t <- sprintf ("AT_A=%.3f(ATH2)%.3f\n mean diff AT_A-ATH2%.2f +/-%.2f", 
+                    coef[2], coef[1], mean(data$AT_A-data$ATH2, na.rm=TRUE),
+                    sd(data$AT_A-data$ATH2, na.rm=TRUE))
     } else {
-      t <- sprintf ("AT_A=%.3f(ATHR2)+%.3f\n mean diff AT_A-ATHR2%.2f +/-%.2f", 
-                    coef[2], coef[1], mean(data$AT_A-data$ATHR2, na.rm=TRUE), 
-                    sd(data$AT_A-data$ATHR2, na.rm=TRUE))
+      t <- sprintf ("AT_A=%.3f(ATH2)+%.3f\n mean diff AT_A-ATH2%.2f +/-%.2f", 
+                    coef[2], coef[1], mean(data$AT_A-data$ATH2, na.rm=TRUE), 
+                    sd(data$AT_A-data$ATH2, na.rm=TRUE))
     }
     title(t, cex.main=0.8)
     if(!labelled) AddFooter (); labelled=T
